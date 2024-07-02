a2f3ccb48ef19578780633bce8281f99fdfa44f1
Https404PaigeNotFound <88682088+Https404PaigeNotFound@users.noreply.github.com>
2024-07-02 12:48:07 +0100

    Reclassify events added to preprocessing

--- PM_4_EEG_memory_load.m
+++ PM_4_EEG_memory_load.m
@@ -15,6 +15,9 @@ eeglab;
 % Add the subdirectory to the path which contain custom functions
 addpath('utils');
 
+% Path to preprocessed data 
+preprocessed_dir = '1_preprocessed_data';
+
 % Paths to each epoch
 epoch_dirs = {'2_epoch_data/probe', '2_epoch_data/memorise', '2_epoch_data/ignore', '2_epoch_data/fixation', '2_epoch_data/maintenance'};
 epoch_conditions = {'probe', 'memorise', 'ignore', 'fixation', 'maintenance'};
@@ -35,47 +38,41 @@ num_bootstraps = 1000;
 % Flag to indicate whether to visualize the results
 visualize = true;
 
-% Get list of files for each condition
-file_lists = cell(length(epoch_conditions), 1);
-for i = 1:length(epoch_conditions)
-    epoch_files = dir(fullfile(pathToEEGLAB, epoch_dirs{i}, '*.set'));
-    file_lists{i} = {epoch_files.name};
-end
 
-% Extract unique participant identifiers
-unique_participants = unique(cellfun(@(x) regexp(x, '^(pilot_Manylabs_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})', 'match', 'once'), file_lists{1}, 'UniformOutput', false));
 %%
 
-% Define memory loads and letter positions % CHECK: The original instructions mention 0-5 letters
-memorise_memory_loads = [3, 5, 7];
-ignore_memory_loads = [4, 6, 8];  % Corresponding to memorise loads + 1
-letter_positions = 0:7;  % 0 to 7 to cover all possible positions
+% Get list of preprocessed files
+preprocessed_files = dir(fullfile(pathToEEGLAB, preprocessed_dir, '*.set'));
 
-% Function to extract event codes from EEG.event structure
-function codes = extract_event_codes(EEG)
-    codes = {EEG.event.type};
+
+
+
+
+
+%%
+
+% Loop over all participants 
+for subj = 1:length(preprocessed_files)
+    % Load subject-specific epoch data
+    EEG = pop_loadset('filename', preprocessed_files(subj).name, 'filepath', fullfile(preprocessed_files(subj).folder));
+    %[ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG, EEG, 0);
+    disp('Data loaded')
+
+   
 end
 
-% Separate memorise and ignore data by memory load and letter position
-[memorise_by_load_pos, ignore_by_load_pos] = deal(cell(length(memorise_memory_loads), length(letter_positions)));
-for load_idx = 1:length(memorise_memory_loads)
-    memorise_load = memorise_memory_loads(load_idx);
-    ignore_load = ignore_memory_loads(load_idx);
-    
-    for pos_idx = 1:length(letter_positions)
-        pos = letter_positions(pos_idx);
-        
-        % For Memorize events
-        memorise_event = sprintf('s%d%d', memorise_load, pos);
-        memorise_indices = find(strcmp(extract_event_codes(memorise), memorise_event));
-        memorise_by_load_pos{load_idx, pos_idx} = memorise.data(:, :, memorise_indices);
-        
-        % For Ignore events
-        ignore_event = sprintf('s%d%d', ignore_load, pos);
-        ignore_indices = find(strcmp(extract_event_codes(ignore), ignore_event));
-        ignore_by_load_pos{load_idx, pos_idx} = ignore.data(:, :, ignore_indices);
-    end
-end
+disp('End of loop')
+
+
+%%
+
+% Re-labeling triggers happens in the preprocessng now 
+
+
+% Compute for a whole (combine 3, 5 & 7). 
+% seperate out the difficulty 3, 5, & 7. 
+
+
 
 % Compute ERSPs for each memory load and letter position
 % ADD
